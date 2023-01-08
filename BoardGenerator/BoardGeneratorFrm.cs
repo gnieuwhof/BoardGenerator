b6663c667b7bf037dcecf0bbb6a9c28872391833
Guido Nieuwhof <guido.nieuwhof@gmail.com>
2023-01-08 21:44:50 +0100

    Menu helper refactoring.

--- BoardGenerator/BoardGeneratorFrm.cs
+++ BoardGenerator/BoardGeneratorFrm.cs
@@ -18,14 +18,14 @@ namespace BoardGenerator
             this.SetStatus("Program started");
         }
 
-        private void SetStatus(string status)
+        public void SetStatus(string status)
         {
-            Logging.Log($"Status updated to: {status}");
-            Logging.EnsureEmptyLine();
-
             string timestamp = DateTime.Now.ToString("HH:mm:ss");
 
             this.toolStripStatusLabel.Text = $"[{timestamp}] {status}";
+
+            Logging.Log($"Status updated to: {status}");
+            Logging.EnsureEmptyLine();
         }
 
         private void QuitMenuItem_Click(object sender, EventArgs e)
@@ -35,84 +35,12 @@ namespace BoardGenerator
 
         private void LoadConfigurationMenuItem_Click(object sender, EventArgs e)
         {
-            Logging.EnsureEmptyLine();
-
-            try
-            {
-                using (var openFileDialog = new OpenFileDialog())
-                {
-                    openFileDialog.Filter = "JSON Configuration (*.json)|*.json";
-                    openFileDialog.RestoreDirectory = true;
-
-                    if (openFileDialog.ShowDialog() == DialogResult.OK)
-                    {
-                        string filePath = openFileDialog.FileName;
-
-                        Stream fileStream = openFileDialog.OpenFile();
-
-                        string fileContent = FileHelper.ReadFromStream(fileStream);
-
-                        var config = Config.Deserialize(fileContent);
-
-                        Logging.Log($"Loaded configuration file: {filePath}");
-
-                        this.SetStatus("Configuration loaded");
-
-                        var bounds = Config.GetBounds(config);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                string error = "An error occurred while loading configuration";
-                this.SetStatus(error);
-                Logging.Log(error);
-                Logging.Log($"{ex}");
-
-                MessageBox.Show("Loading configuration failed.",
-                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            MenuHelper.LoadConfiguration(this);
         }
 
         private void CreateConfigurationExampleMenuItem_Click(object sender, EventArgs e)
         {
-            Logging.EnsureEmptyLine();
-
-            var example = Config.CreateExample();
-
-            try
-            {
-                var sfd = new SaveFileDialog();
-                sfd.Filter = "JSON Configuration|*.json";
-                sfd.Title = "Save Configuration File";
-                sfd.ShowDialog();
-
-                if (sfd.FileName != "")
-                {
-                    var fs = (System.IO.FileStream)sfd.OpenFile();
-
-                    string json = Config.Serialize(example);
-
-                    FileHelper.WriteToStream(fs, json);
-
-                    fs.Close();
-
-                    Logging.Log($"Configuration saved to: {sfd.FileName}");
-
-                    this.SetStatus("Example configuration saved");
-                }
-                else
-                {
-                    this.SetStatus("Creating example configuration was cancelled");
-                }
-            }
-            catch (Exception ex)
-            {
-                string error = "An error occurred while saving configuration";
-                this.SetStatus(error);
-                Logging.Log(error);
-                Logging.Log($"{ex}");
-            }
+            MenuHelper.CreateConfigurationExample(this);
         }
 
         private void OpenLogMenuItem_Click(object sender, EventArgs e)
---
+++ BoardGenerator/MenuHelper.cs
@@ -0,0 +1,93 @@
+﻿namespace BoardGenerator
+{
+    using System;
+
+    internal static class MenuHelper
+    {
+        public static void LoadConfiguration(BoardGeneratorFrm frm)
+        {
+            _ = frm ?? throw new ArgumentNullException(nameof(frm));
+
+            Logging.EnsureEmptyLine();
+
+            try
+            {
+                using (var openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "JSON Configuration (*.json)|*.json";
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog.FileName;
+
+                        Stream fileStream = openFileDialog.OpenFile();
+
+                        string fileContent = FileHelper.ReadFromStream(fileStream);
+
+                        var config = Config.Deserialize(fileContent);
+
+                        Logging.Log($"Loaded configuration file: {filePath}");
+
+                        frm.SetStatus("Configuration loaded");
+
+                        var bounds = Config.GetBounds(config);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = "An error occurred while loading configuration";
+                frm.SetStatus(error);
+                Logging.Log(error);
+                Logging.Log($"{ex}");
+
+                MessageBox.Show("Loading configuration failed.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CreateConfigurationExample(BoardGeneratorFrm frm)
+        {
+            _ = frm ?? throw new ArgumentNullException(nameof(frm));
+
+            Logging.EnsureEmptyLine();
+
+            var example = Config.CreateExample();
+
+            try
+            {
+                var sfd = new SaveFileDialog();
+                sfd.Filter = "JSON Configuration|*.json";
+                sfd.Title = "Save Configuration File";
+                sfd.ShowDialog();
+
+                if (sfd.FileName != "")
+                {
+                    var fs = (System.IO.FileStream)sfd.OpenFile();
+
+                    string json = Config.Serialize(example);
+
+                    FileHelper.WriteToStream(fs, json);
+
+                    fs.Close();
+
+                    Logging.Log($"Configuration saved to: {sfd.FileName}");
+
+                    frm.SetStatus("Example configuration saved");
+                }
+                else
+                {
+                    frm.SetStatus("Creating example configuration was cancelled");
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = "An error occurred while saving configuration";
+                frm.SetStatus(error);
+                Logging.Log(error);
+                Logging.Log($"{ex}");
+            }
+        }
+    }
+}
